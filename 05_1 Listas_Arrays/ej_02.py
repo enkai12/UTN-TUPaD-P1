0ae0f285b519ca4f0add8c7af80a7309d1fe36fe
Agustín Sotelo <agustinemiliano22@gmail.com>
2025-05-19 10:43:53 -0300

    tp_05_1 Se agregan ejercicio 2 y 3 resueltos

---
+++ 05_1 Listas_Arrays/ej_02.py
@@ -0,0 +1,7 @@
+# 2) Crear una lista con cinco elementos (colocar los elementos que más te gusten) y mostrar el
+# penúltimo. ¡Puedes hacerlo como se muestra en los videos o bien investigar cómo funciona el
+# indexing con números negativos!
+
+elementos = ["Chocolates", "Helado", "Bailar", "Perros", "Pizza"]
+
+print(elementos[-2])
